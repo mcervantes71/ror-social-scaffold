4725819a4f8eeb2787770aca2b4ea38d0cdccd74
Aulbourn <aulbourn@gmail.com>
2020-07-07 09:10:46 -0400

    add validation test for comment

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -4,4 +4,16 @@ FactoryBot.define do
     post_id {1}
     content {"Awesome post!"}
   end
+
+  trait :post do
+    post_id {1}
+  end
+
+  trait :commenter do
+    user_id {1}
+  end
+
+  trait :invalid_comment do
+    content {''}
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:invalid_comment) {FactoryBot.build(:comment, :invalid_comment)}
+
+  describe 'validations for comment model' do
+    it 'is not valid if the comment exceeds limit ' do
+      should_not allow_value('this is a very long tape' * 20).for(:content)
+    end
+
+    it 'is not valid without a post' do
+      should_not allow_value(invalid_comment.post).for(:post)
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to user' do
+      should belong_to(:user)
+    end
+
+    it 'belongs to post' do
+      should belong_to(:post)
+    end
+  end
   end
