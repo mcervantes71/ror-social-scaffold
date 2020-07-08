700191fe50ff68b889890e6d5e8f23e57ca464ef
Aulbourn <aulbourn@gmail.com>
2020-07-08 11:42:48 -0400

    fix rubocop lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,33 +13,33 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    friendships.map { |friendship| friendship.friend if friendship.confirmed } .compact
+    friendships.map { |friendship| friendship.friend if friendship.confirmed }.compact
   end
 
   def inverse_friends
-    inverse_friendships.map { |friendship| friendship.user if friendship.confirmed } .compact
+    inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }.compact
   end
 
   # Users who have yet to confirme friend requests
   def pending_friend(user)
     inverse_friendships.map do |friendship|
       friendship.user.id if friendship.user_id == user.id && !friendship.confirmed
-    end .compact
+    end.compact
   end
 
   def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed } .compact
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
   # Users who have requested to be friends
   def friend_request(user)
     friendships.map do |friendship|
       friendship.friend.id if friendship.friend_id == user.id && !friendship.confirmed
-    end .compact
+    end.compact
   end
 
   def friend_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed } .compact
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
 
   def friend?(user)
