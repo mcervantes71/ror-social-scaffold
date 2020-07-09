fd420569c85410422c94eb15ba57d9d63b616db2
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-08 20:50:34 -0600

    Fix pending friend association

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,15 @@ class User < ApplicationRecord
   has_many :confirmed_friendships, -> { where status: 'Confirmed' }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
+  has_many :pending_friendships, -> { where status: 'Pending' }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
   def friends
     friendships.map { |friendship| friendship.friend if friendship.status == 'Confirmed' } .compact
   end
 
-  def pending_friend(current_user, user)
-    if current_user.friendships.where(status: 'Pending').find_by(friend_id: user.id)
-      true
-    else
-      false
-    end
+  def pending_friend(user)
+    pending_friendships.any? { |friendship| friendship.friend == user }
   end
 
   def friend_request(current_user, user)
